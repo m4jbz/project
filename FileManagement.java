3f1f0e5b599e7186978201a1fd8ce7adc54aa92d
m4jbz <zeroknel@proton.me>
2024-04-16 21:04:56 -0600

    add option works

--- FileManagement.java
+++ FileManagement.java
@@ -5,13 +5,51 @@ import java.io.*;
 
 class FileManagement extends Encrypt
 {
-	static int numAcc;
     static final String key = "papupapu";
 	static Scanner sc = new Scanner(System.in);
 
-	public void deleteFiles(String[] files)
+	public int numberOfFiles()
 	{
-		for (int i = 0; i < numAcc; ++i) {
+		int cont = 0, i = 0;
+		boolean flag = true;
+
+		while (flag) {
+			File fl = null;
+			if ((i+1) > 9)
+				fl = new File(String.format("files/accrypted%d.bin", (i+1)));
+			else 
+				fl = new File(String.format("files/accrypted0%d.bin", (i+1)));
+			i++;
+			if (fl.exists()) cont++;
+			else flag = false;
+		}
+
+		return cont;
+	}
+
+	public void addAccount()
+	{
+		String[] acc = listOfFiles(numberOfFiles());
+		int lastNum = 0;
+
+		if (acc.length > 0 && acc.length < 10)
+			lastNum = Character.getNumericValue(acc[acc.length-1].charAt(16));
+		else if (acc.length > 0 && acc.length > 9)
+			lastNum = Integer.parseInt(String.format("%c%c", acc[acc.length-1].charAt(15) + acc[acc.length-1].charAt(16)));
+
+		System.out.print("Numero de cuentas: ");
+		int numAcc = sc.nextInt();
+
+		String[] accounts = requestData(lastNum, lastNum+numAcc, numAcc);
+		String[] encryptedAccounts = listOfBinFiles(accounts, lastNum, lastNum+numAcc);
+
+		makeBinFiles(accounts, encryptedAccounts, numAcc);
+		deleteFiles(accounts, accounts.length);
+	}
+
+	public void deleteFiles(String[] files, int n)
+	{
+		for (int i = 0; i < n; ++i) {
 			File fl = new File(files[i]);
 
 			if (fl.exists()) {
@@ -24,74 +62,58 @@ class FileManagement extends Encrypt
 		}
 	}
 
-	public void verifyFilesExistence()
-	{
-		int cont = 0, i = 0;
-		boolean flag = true;
-
-		while (flag) {
-			File fl = new File(String.format("files/account%d_encrypted.bin", (i+1)));
-			i++;
-
-			if (fl.exists())
-				cont++;
-			else
-				flag = false;
-		}
-
-		if (cont > 0)
-			showAccounts(listOfFiles(cont));
-		else 
-			System.out.println("No hay cuentas por mostrar.");
-	}
-
 	public String[] listOfFiles(int a)
 	{
 		String[] str = new String[a];
 		
-		for(int i = 0; i < a; ++i)
-			str[i] = String.format("files/account%d_encrypted.bin", (i+1));
+		for (int i = 0; i < a; ++i)
+			if ((i+1) > 9)
+				str[i] = String.format("files/accrypted%d.bin", (i+1));
+			else 
+				str[i] = String.format("files/accrypted0%d.bin", (i+1));
 
 		return str;
 	}
 
 	public void showAccounts(String[] str)
 	{
-		for (int i = 0; i < str.length; ++i) {
-			try {
-				SecretKey secretKey = generateSecretKey(key);
+		if (str.length > 0) {
+			for (int i = 0; i < str.length; ++i) {
+				try {
+					SecretKey secretKey = generateSecretKey(key);
 
-				String decryptedContent = decryptedFile(str[i], secretKey);
-				System.out.println("--- --- --- --- ---");
-				System.out.println(String.format("Cuenta %d: ", (i+1)));
-				System.out.println(decryptedContent);
-			} catch (IOException | GeneralSecurityException e) {
-				e.printStackTrace();
+					String decryptedContent = decryptedFile(str[i], secretKey);
+					System.out.println("--- --- --- --- ---");
+					System.out.println(String.format("Cuenta %d: ", (i+1)));
+					System.out.println(decryptedContent);
+				} catch (IOException | GeneralSecurityException e) {
+					e.printStackTrace();
+				}
 			}
-		}
-		System.out.println("--- --- --- --- ---");
+			System.out.println("--- --- --- --- ---");
+		} else System.out.println("No hay cuentas por mostrar.");
 	}
 
-
-	public String[] requestData()
+	public String[] requestData(int n, int m, int numAcc)
 	{
-		System.out.print("Numero de cuentas: ");
-		numAcc = sc.nextInt();
-
+		int a = 0;
 		String[] accounts = new String[numAcc];
 		String[] mails = new String[numAcc];
 		String[] passwds = new String[numAcc];
 
-		for (int i = 0; i < numAcc; ++i) {
-			System.out.print(String.format("%d° Correo: ", (i+1)));
-			mails[i] = sc.next();
-			System.out.print(String.format("%d° Contraseña: ", (i+1)));
-			passwds[i] = sc.next();
-
-			accounts[i] = String.format("files/account%d.txt", (i+1));
+		for (int i = n; i < m; ++i) {
+			System.out.print(String.format("%d° Correo: ", (a+1)));
+			mails[a] = sc.next();
+			System.out.print(String.format("%d° Contraseña: ", (a+1)));
+			passwds[a] = sc.next();
+			if ((i+1) > 9)
+				accounts[a] = String.format("files/account%d.txt", (i+1));
+			else 
+				accounts[a] = String.format("files/account0%d.txt", (i+1));
+			a++;
 		}
 
-		for (int i = 0; i < numAcc; ++i) {
+		for (int i = 0; i < m-n; ++i) {
 			try (BufferedWriter writer = new BufferedWriter(new FileWriter(accounts[i], false))) {
 				writer.write(mails[i]);
 				writer.newLine();
@@ -104,15 +126,47 @@ class FileManagement extends Encrypt
 		return accounts;
 	}
 
-	public String[] makeBinFiles(String[] accounts)
+	public String[] listOfBinFiles(String[] accounts, int n, int m)
 	{
-		String[] encryptedFiles = new String[numAcc];
+		String[] encryptedFiles = new String[m-n];
+		int a = 0;
 		
-		for (int i = 0; i < numAcc; ++i) {
-			encryptedFiles[i] = String.format("files/account%d_encrypted.bin", (i+1));
+		for (int i = n; i < m; ++i) {
+			if ((i+1) > 9)
+				encryptedFiles[a] = String.format("files/accrypted%d.bin", (i+1));
+			else 
+				encryptedFiles[a] = String.format("files/accrypted0%d.bin", (i+1));
+			a++;
 		}
 
 		return encryptedFiles;
+	}
+
+	public void showContent(String[] encryptedAccounts)
+	{
+		for (int i = 0; i < 1; ++i) {
+			try {
+				SecretKey secretKey2 = generateSecretKey(key);
+
+				String decryptedContent = decryptedFile(encryptedAccounts[i], secretKey2);
+				System.out.println(String.format("Contenido %d: ", (i+1)));
+				System.out.println(decryptedContent);
+			} catch (IOException | GeneralSecurityException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void makeBinFiles(String[] accounts, String[] encryptedAccounts, int n)
+	{
+		for (int i = 0; i < n; ++i) {
+			try {
+				SecretKey secretKey = generateSecretKey(key);
+				encryptFile(accounts[i], encryptedAccounts[i], secretKey);
+			} catch (IOException | GeneralSecurityException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,4 @@
-import java.security.GeneralSecurityException;
-import javax.crypto.SecretKey;
 import java.util.Scanner;
-import java.io.*;
 
 public class Main
 {
@@ -9,8 +6,6 @@ public class Main
 	{
 		Scanner sc = new Scanner(System.in);
 		FileManagement fm = new FileManagement();
-		String[] accounts;
-		String[] encryptedFiles;
 
 		byte options = 0;
 
@@ -26,7 +21,10 @@ public class Main
 
 			switch (options) {
 				case 1:
-					fm.verifyFilesExistence();
+					fm.showAccounts(fm.listOfFiles(fm.numberOfFiles()));;
+					break;
+				case 2:
+					fm.addAccount();
 					break;
 				case 5:
 					System.out.println("¡Adiós!");
@@ -37,30 +35,6 @@ public class Main
 
 			sc.nextLine();
 		} while (options != 5);
-
-		accounts = fm.requestData();
-		encryptedFiles = fm.makeBinFiles(accounts);
-
-		for (int i = 0; i < fm.numAcc; ++i) {
-			try {
-				SecretKey secretKey = fm.generateSecretKey(fm.key);
-				fm.encryptFile(accounts[i], encryptedFiles[i], secretKey);
-			} catch (IOException | GeneralSecurityException e) {
-				e.printStackTrace();
-			}
-
-			try {
-				SecretKey secretKey2 = fm.generateSecretKey(fm.key);
-
-				String decryptedContent = fm.decryptedFile(encryptedFiles[i], secretKey2);
-				System.out.println(String.format("Contenido %d: ", (i+1)));
-				System.out.println(decryptedContent);
-			} catch (IOException | GeneralSecurityException e) {
-				e.printStackTrace();
-			}
-
-		}
-		fm.deleteFiles(accounts);
 
 		sc.close();
     }
